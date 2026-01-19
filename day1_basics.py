4e65f5371052d8481d6ca5ae51c3edd68d0db570
Kim <kim16machado@gmail.com>
2026-01-19 08:35:30 -0600

    Day 1: Python basics

---
+++ day1_basics.py
@@ -0,0 +1,31 @@
+name = input("Whats your name: ")
+#age = int(input("Enter your age: "))
+
+name = name.title().strip()
+
+print("Hello", name, end='\n')
+print(f"Hello {name}")
+#print( "You are", age, "years old")
+
+#Calculator
+#a = int(input("Enetr first number: "))
+#b = int(input("Enter second number: "))
+
+
+#add = a+b
+#print(add)
+#subtract = a-b
+#print(subtract)
+#multiplication = a*b
+#print(multiplication)
+#division = a/b
+#print(division)
+
+#Temperature converter
+
+"""
+temp = float(input("Enetr your temperature in degree celsius: "))
+farenheit = (temp * 9/5) + 32
+
+print("Temperature in farenheit is", farenheit)
+"""
