1384b9599c41121f089e8cd91e3f98aa6d97c49c
Kim <kim16machado@gmail.com>
2026-01-20 21:14:20 -0600

    Password checker update

--- day2_strings_slicing.py
+++ day2_strings_slicing.py
@@ -10,7 +10,7 @@ print("Word Count", len(string.split()))
 """
 
 #Extracting info
-
+"""
 email= str(input("enter your email: "))
 
 username= email[:email.index("@")]
@@ -18,3 +18,43 @@ domain = email[email.index("@")+1:]
 
 print(username)
 print(domain)
+"""
+
+#Palindrome
+"""
+userinput = input("Enter your input: ")
+
+clean = userinput.lower().strip()
+
+if clean == clean[::-1]:
+    print("Its palindrome")
+else:
+    print("Its not")  
+"""
+
+#password checker
+
+password =  (input("Enter your password: "))
+
+has_digit = False
+has_upper = False
+has_lower = False
+
+if len(password) < 8:
+    print("Invalid password")
+else:
+    for i in password:
+        if i.isdigit():
+           has_digit = True
+        elif i.isupper():
+           has_upper = True
+        elif i.islower():
+           has_lower = True
+
+    if has_digit and has_upper and has_lower:
+        print("Strong password")
+    else:
+        print("Weak password")           
+
+
+               
