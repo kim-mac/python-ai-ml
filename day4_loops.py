688d2cbf37454e3fdbff0968dd999dfc1df35f78
Kim <kim16machado@gmail.com>
2026-01-22 16:13:40 -0600

    Day4: Loops and Patterns

---
+++ day4_loops.py
@@ -0,0 +1,30 @@
+#Multiplication
+"""
+num = int(input("Enter your number"))
+
+for i in range(1,11):
+    print(f"{num} * {i} = {num * i}")
+"""
+
+#Sum of numbers
+"""
+total = 0
+
+for i in range(1,101):
+    total += i
+
+print(total)
+"""
+
+#pattern
+pattern = input("Input your symbol: ")
+
+for i in range(1,6):
+    print(f"{pattern * i}")
+
+#pattern reverse
+
+pattern = input("Input your symbol: ")
+
+for i in range(5,0,-1):
+    print(f"{pattern * i}")
