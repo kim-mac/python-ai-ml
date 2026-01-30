241d6f471de9444f9fdfb6a3231d1f0a3c914768
Kim <kim16machado@gmail.com>
2026-01-29 22:48:31 -0600

    test

--- day5_list_tuples.py
+++ day5_list_tuples.py
@@ -1,6 +1,22 @@
+"""
 symbol = input("Input your symbol: ")
 
 for i in range(1, 6):
     for j in range(i):
         print(symbol, end="")
     print()
+"""
+
+marks = [1,2,3]
+
+for i in range(5):
+    score = int(input(f"Enter marks for student {i+1}: "))
+    marks.append(score)
+
+print("Marks:", marks)
+print("Average:", sum(marks) / len(marks))
+print("Highest:", max(marks))
+print("Lowest:", min(marks))
+
+
+
