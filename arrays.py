de02a3ce63f197fcf10c2aa7056f99af1e5ff00e
Kim <kim16machado@gmail.com>
2026-02-05 16:21:57 -0600

    arrays exercise

---
+++ arrays.py
@@ -0,0 +1,33 @@
+#exercise
+
+expense_data = [2200,2350,2600,2130,2190]
+
+print("i spent",expense_data[2]-expense_data[1], "extra")
+
+print("Total expense in first three months is", expense_data[0]+expense_data[1]+expense_data[2])
+
+print("Did i spent 2000 in any months", 2000 in expense_data)
+
+expense_data.append(1980)
+print("spending for the month of june", expense_data)
+
+expense_data[3] -= 200
+print("New data", expense_data)
+
+#exercise 2
+heros=['spider man','thor','hulk','iron man','captain america']
+
+print("The length of th list is", len(heros))
+
+heros.append("black panther")
+print("new list is", heros)
+
+heros.remove("black panther")
+heros.insert(3,"black panther")
+print(heros)
+
+heros[1:3] = ["Doctor strange"]
+print(heros)
+
+heros.sort()
+print(heros)
--- day5_list_tuples.py
+++ day5_list_tuples.py
@@ -6,7 +6,7 @@ for i in range(1, 6):
         print(symbol, end="")
     print()
 """
-
+"""
 marks = []
 
 for i in range(5):
@@ -17,6 +17,24 @@ print("Marks:", marks)
 print("Average:", sum(marks) / len(marks))
 print("Highest:", max(marks))
 print("Lowest:", min(marks))
+"""
+"""
+def main():
+    print(f"{r}")
+    arr() 
 
+def arr(r):
+    arrays = [1,2,3,4,5]
 
+    for i in range(len(arrays)):
+       if arrays[i] == 3:
+         return i 
+
+main()
+"""
+#exercise
+
+expense_data = [2200,2350,2600,2130,2190]
+
+print("i spent",expense_data[2]-expense_data[1], "extra")
 
