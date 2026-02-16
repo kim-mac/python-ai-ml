032d13bc347b2e3278971ae7040c09f9270c4994
Kim <kim16machado@gmail.com>
2026-02-15 19:44:18 -0600

    API updates

--- tryapi.py
+++ tryapi.py
@@ -5,7 +5,7 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])    
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])    
 
 song = response.json()
 for result in song["results"]:
