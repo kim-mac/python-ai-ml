fb068084c09fcb4f17f2fa3fa973ff1cecddfb2b
Kim <kim16machado@gmail.com>
2026-03-13 19:04:47 -0500

    more oop

--- oop.py
+++ oop.py
@@ -1,18 +1,30 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError
         if house not in ["h","p","q","d"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
+
 
     def __str__(self):
         return f"{self.name} is in {self.house}"
 
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "Right Stag"
+            case "Dog":
+                return "Right Dog"
+            case _:
+                return "Nothing"
+
 def main():
     student = get_student()
-    print(student)
+    print("Expecto Petronium")
+    print(student.charm)
 
 
 def get_student():
@@ -29,8 +41,9 @@ def get_student():
 
     #method 3 using class and objects
     name = input("Name: ")
-    house = input("House:")  
-    return Student(name, house)                #objects are mutable but can be made immutable            
+    house = input("House: ")
+    patronus = input("Petronus: ")  
+    return Student(name, house, patronus)                #objects are mutable but can be made immutable            
     
 
 
